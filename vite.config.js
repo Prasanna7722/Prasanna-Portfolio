445db60408fccdc38b820f7717a09cb74a676f57
Prasanna7722 <reddyprasannaguntimadugu@gmail.com>
2025-08-22 09:44:01 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Prasanna_Portfolio/",
+  base: "/Prasanna-Portfolio/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
